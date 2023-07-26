776f5a10dd3e7e066811175006518f00f3e177ea
Linvmboya <linvmboya@gmail.com>
2023-07-26 12:30:27 -0700

    update

--- main.h
+++ main.h
@@ -7,5 +7,22 @@
 
 int _printf(const char *format, ...);
 
+/*functions for specifiers*/
+int get_flags(const char *format, int *m);
+int get_width(const char *format, int *m, va_list list);
+int get_precision(const char *format int *m, va_list list);
+int get_len(const char *format, int *m);
+
+/*handler for width*/
+int handle_write_char(char c, char buffer[], int flags,
+		int width, int precision, int len);
+int write_number(int is positive, int buff, char buffer[],
+		int flags, int width, int precision, int len);
+int write_num(int buff, char bff[], int flags, int width,
+		int precision, int length, char padd, char extra_c);
+int write_pointers(char buffer[], int buff, int length, int width,
+		int flags, char padd, char extra_c int padd_start);
+int write_unsigned(int is_negative, int buff, char buffer[],
+		int flags, int width, int precision, int len);
 
 #endif
